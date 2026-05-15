62730d8b26bee93834e30709846d72609d934d16
nluna19 <nluna190898@gmail.com>
2026-05-15 02:23:58 -0300

    drag-drop.component. input and output

--- src/app/shared/components/drag-and-drop/drag-drop.component.ts
+++ src/app/shared/components/drag-and-drop/drag-drop.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, input, OnInit, output } from '@angular/core';
 import { DragDropModule } from 'primeng/dragdrop';
 
 const SourceList = {
@@ -18,34 +18,40 @@ interface DragDropListItem {
   imports: [CommonModule, DragDropModule],
   templateUrl: './drag-drop.component.html',
 })
-export class DragDropComponent {
-  draggedItem: any = null;
+export class DragDropComponent implements OnInit {
+  // TODO: usar data binding bidireccional.
+  leftItems = input<DragDropListItem[]>([]);
+  rightItems = input<DragDropListItem[]>([]);
+  onDrop = output<DragEvent>();
+  onChange = output<{ left: DragDropListItem[]; right: DragDropListItem[] }>();
 
-  left: DragDropListItem[] = [
-    { id: 1, label: 'Juan' },
-    { id: 2, label: 'Ana' },
-    { id: 3, label: 'Pedro' },
-  ];
-
+  left: DragDropListItem[] = [];
   right: DragDropListItem[] = [];
-
+  draggedItem: any = null;
   dragFrom: SourceList | null = null;
+
+  ngOnInit() {
+    this.left = this.leftItems();
+    this.right = this.rightItems();
+  }
 
   dragStart(item: DragDropListItem, from: SourceList) {
     this.draggedItem = item;
     this.dragFrom = from;
   }
 
-  drop(to: SourceList) {
+  drop(event: DragEvent, to: SourceList) {
+    this.onDrop.emit(event);
     if (this.draggedItem) {
       if (to === SourceList.left && this.dragFrom === SourceList.right) {
-        this.left = [...this.left, this.draggedItem];
-        this.right = this.right!.filter((i: DragDropListItem) => i.id !== this.draggedItem.id);
+        this.left.push(this.draggedItem);
+        this.right = this.right.filter((i: DragDropListItem) => i.id !== this.draggedItem.id);
       } else if (to === SourceList.right && this.dragFrom === SourceList.left) {
-        this.right = [...this.right, this.draggedItem];
-        this.left = this.left!.filter((i: DragDropListItem) => i.id !== this.draggedItem.id);
+        this.right.push(this.draggedItem);
+        this.left = this.left.filter((i: DragDropListItem) => i.id !== this.draggedItem.id);
       }
     }
+    this.onChange.emit({ left: this.left, right: this.right });
   }
 
   dragEnd() {
